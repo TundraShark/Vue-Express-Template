15df548f075abef1d1423b90c47a8d261cb085a6
Tundra Fizz <coleman.leif@gmail.com>
2019-07-29 15:50:54 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ var yaml         = require("js-yaml");              // Parse .yaml files
 var mysql        = require("mysql2");               // MySQL Database
 var bodyParser   = require("body-parser");          // Allows you to read POST data
 var cookieParser = require("cookie-parser");        // Cookies
-var cors         = require("cors");
+var cors         = require("cors");                 // Cross-origin resource sharing
 var app          = module.exports = express();      // Define the application
 
 var config = yaml.safeLoad(fs.readFileSync("config.yml", "utf-8"));
@@ -13,10 +13,6 @@ var config = yaml.safeLoad(fs.readFileSync("config.yml", "utf-8"));
 var mode = process.env.mode || "local";
 
 app.set("views", "./views");                         // Define the views directory
-// app.use(sass({src:   __dirname + "/static/css/sass", // Set SASS directory for the source
-//               dest:  __dirname + "/static/css",      // Set SASS directory for the destination
-//               prefix: "/css",                        // Set SASS prefix
-//               outputStyle: "compressed"}));          // Set SASS compression
 app.use(express.static("./static"));                 // Define the static directory
 app.use(bodyParser.json());                          // Setting for bodyParser
 app.use(bodyParser.urlencoded({extended: true}));    // Setting for bodyParser
