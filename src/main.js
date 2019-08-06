4be5b6a614c5feba58a0d34d61016d7532ba5f32
Tundra Fizz <coleman.leif@gmail.com>
2019-08-05 23:25:04 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import Router from "vue-router";
+import Axios from "axios";
 import App from "./routes/app.vue";
 import Home from "./routes/home.vue";
 import About from "./routes/about.vue";
@@ -23,13 +24,34 @@ const router = new Router({
   }]
 });
 
-// Define basic functions that allow easy usage of reading and writing from the data store
+// Everything defined in Vue.mixin will be created in every Vue instance, effectively making these global methods and variables
 Vue.mixin({
+  // Define basic functions that allow easy usage of reading and writing from the data store
   methods: {
     Read : function(key     ){return this.$store.state[key]},
-    Write: function(key, val){this.$store.commit(key, val)}
+    Write: function(key, val){this.$store.commit(key, val)},
+
+    Get: async function(endpoint) {
+      let response = await this.Axios.get(`${process.env.VUE_APP_BACKEND}/${endpoint}`, {withCredentials: true});
+      return response["data"];
+    },
+    Post: async function(endpoint, body = {}) {
+      let response = await this.Axios.post(`${process.env.VUE_APP_BACKEND}/${endpoint}`, body, {withCredentials: true});
+      return response["data"];
+    },
+    Put: async function(endpoint, body = {}) {
+      let response = await this.Axios.put(`${process.env.VUE_APP_BACKEND}/${endpoint}`, body, {withCredentials: true});
+      return response["data"];
+    },
+    Delete: async function(endpoint) {
+      let response = await this.Axios.delete(`${process.env.VUE_APP_BACKEND}/${endpoint}`, {withCredentials: true});
+      return response["data"];
+    }
   },
-  data: function(){return {API: process.env.VUE_APP_BACKEND}}
+  created: function() {
+    this.API = process.env.VUE_APP_BACKEND;
+    this.Axios = Axios;
+  }
 });
 
 // Define data that will be used across the entire application
