6f6c45093d45bf663c88a4a1bf5a9827585fe3ab
Tundra Fizz <coleman.leif@gmail.com>
2019-08-11 21:26:47 -0700

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -7,9 +7,8 @@ axios.defaults.validateStatus = false;
 
 app.use(async function(req, res, next) {
   config["secretToken"] = "^VVdum87ujqqxUSF@C*ofA6d3c#k*vxje6&g7iXtNgLnVvj#Kl%uDq!9YbdE2FGI";
-  console.log(">>>", config["secretToken"]);
-  console.log(">>>", req["cookies"]["secret-token"]);
 
+  // Authenticate the client
   if(req["cookies"]["secret-token"] == config["secretToken"]) {
     next();
   } else {
@@ -18,17 +17,14 @@ app.use(async function(req, res, next) {
 });
 
 app.get("/data", async (req, res) => {
-  let [results] = await db.query("SELECT * FROM sample_table");
+  let results = await db.GetData();
   res.json(results);
 });
 
 app.post("/data", async (req, res) => {
   let name = req["body"]["name"];
   let age  = req["body"]["age"];
-  let sql  = "INSERT INTO sample_table (name, age) VALUES (?, ?)";
-  let args = [name, age];
-  await db.query(sql, args);
-  let [results] = await db.query("SELECT * FROM sample_table");
+  let results = await db.PostData(name, age);
   res.json(results);
 });
 
@@ -36,19 +32,13 @@ app.put("/data", async (req, res) => {
   let id   = req["body"]["id"];
   let name = req["body"]["name"];
   let age  = req["body"]["age"];
-  let sql  = "UPDATE sample_table SET name=?, age=? WHERE id=?";
-  let args = [name, age, id];
-  await db.query(sql, args);
-  let [results] = await db.query("SELECT * FROM sample_table");
+  let results = await db.PutData(id, name, age);
   res.json(results);
 });
 
 app.delete("/data", async (req, res) => {
-  let id   = req["query"]["id"];
-  let sql  = "DELETE FROM sample_table WHERE id=?";
-  let args = [id];
-  await db.query(sql, args);
-  let [results] = await db.query("SELECT * FROM sample_table");
+  let id = req["query"]["id"];
+  let results = await db.DeleteData(id);
   res.json(results);
 });
 
