913501acebfc5936f132cbcad6ce3583b2e74ef1
Tundra Fizz <coleman.leif@gmail.com>
2019-08-11 21:26:35 -0700

    Add db.js

---
+++ backend/db.js
@@ -0,0 +1,48 @@
+const mysql = require("mysql2");
+
+class DB {
+  constructor(data) {
+    this.db = mysql.createPool({
+      host    : data["host"],
+      user    : data["user"],
+      password: data["password"],
+      database: data["database"]
+    }).promise();
+  }
+
+  async Query(sql, args) {
+    try {
+      return (await this.db.query(sql, args))[0];
+    } catch(error) {
+      return {error: `${error["code"]} [${error["errno"]}]: ${error["message"]}`};
+    }
+  }
+
+  async GetData() {
+    let results = await this.Query("SELECT * FROM sample_table");
+    return results;
+  }
+
+  async PostData(name, age) {
+    let results = await this.Query("INSERT INTO sample_table (name, age) VALUES (?, ?)", [name, age]);
+    console.log("====================================");
+    console.log(results);
+    return results;
+  }
+
+  async PutData(id, name, age) {
+    let results = await this.Query("UPDATE sample_table SET name=?, age=? WHERE id=?", [name, age, id]);
+    console.log("====================================");
+    console.log(results);
+    return results;
+  }
+
+  async DeleteData(id) {
+    let results = await this.Query("DELETE FROM sample_table WHERE id=?", [id]);
+    console.log("====================================");
+    console.log(results);
+    return results;
+  }
+};
+
+module.exports = DB;
